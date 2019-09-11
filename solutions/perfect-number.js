fc9cdd0d3a33bae568be80b446b5cd3bf2c938fc
Sergey Averyanov <railtrailtime@gmail.com>
2019-09-11 14:48:57 +0300

    Added another solution for perfect-number

--- solutions/perfect-number.js
+++ solutions/perfect-number.js
@@ -14,3 +14,18 @@ var checkPerfectNumber = function(num) {
     if (!divisors.length) return false;
     return num === divisors.reduce((a, b) => a + b);
 };
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber2 = function(num) {
+    var sum = 0;
+    for (var divisor = 1; divisor < num; divisor++) {
+        if (num % divisor === 0) {
+            sum += divisor;
+            if (sum > num) return false;
+        }
+    }
+    return sum == num;
+};
