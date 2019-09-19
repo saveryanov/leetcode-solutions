d514aead525342e171802abb8c3a53e5ea4c08c1
Sergey Averyanov <railtrailtime@gmail.com>
2019-09-19 12:26:31 +0300

    Added solution for max-area-of-island

---
+++ solutions/max-area-of-island.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/surrounded-regions/
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+function maxAreaOfIsland(grid) {
+    let height = grid.length;
+    if (!height) return grid;
+    let width = grid[0].length;
+    if (!width) return grid;
+
+    function checkIsland(x, y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) return 0;
+        if (grid[y][x] === 0) return 0;
+        grid[y][x] = 0;
+        let area = 1;
+        area += checkIsland(x - 1, y);
+        area += checkIsland(x + 1, y);
+        area += checkIsland(x, y - 1);
+        area += checkIsland(x, y + 1);
+        return area;
+    }
+
+    let maxArea = 0;
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            if (grid[y][x] === 1) {
+                let area = checkIsland(x, y);
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+    }
+
+    return maxArea;
+}
